0f122b7f15a33cee2b75d31cda930a50c3a7a814
peshne <anurag_peshne@yahoo.co.in>
2013-02-17 10:53:34 +0530

    initial commit

---
+++ backend/Disk.java
@@ -0,0 +1,82 @@
+package backend;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class Disk {
+	
+	public static final File homeDir = new File(System.getProperty("user.home"));	
+	public static final File transDisk = new File(homeDir.toString() + "/TransDisk");;
+	
+	private static final int partitionTableAddress = 0;
+	/*
+	public Disk(){
+		createRoot();
+		createBlocks();
+	}*/
+	public static void createDisk(){
+		createRoot();
+		createBlocks();
+		makePartitionTable();
+	}
+	
+	private static void createRoot(){
+		if(!transDisk.exists()){
+			if(transDisk.mkdir()){
+				System.out.println("root created");
+			}
+			else{
+				System.out.println("root creation failed");
+			}
+		}
+		else{
+			System.out.println(transDisk.length());
+			System.out.println("root dir present");
+		}
+	}
+	
+	private static void createBlocks(){
+		int noOfBlocks = kickStart.diskSize*1000/kickStart.maxBlockSize;
+		for(int i = 0; i < noOfBlocks; i++){
+			File f = new File(homeDir.toString() + "/TransDisk/" + String.format("%05d", i));
+			if(!f.exists()){
+				try {
+					f.createNewFile();	
+					System.out.println("\t"+i+ " created");
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			else{
+				System.out.println("block present");
+			}
+		}
+	}
+	private static void makePartitionTable(){
+		try {
+			String content;// = "//Partition Table\n//Status\tStart\tEnd";			//4th entry omitted
+			
+			File partitionTable = new File(Disk.transDisk + "/" +String.format("%05d", Disk.partitionTableAddress));
+ 
+			if (!partitionTable.exists()) {
+				System.out.println("Partition Table Not found");
+			}
+			
+			//_________Hard coding partition___________________
+			content = "\n11111\t" + String.format("%05d", 1) + "\t" + String.format("%05d", 10);
+			
+			FileWriter fw = new FileWriter(partitionTable.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(content);
+			bw.close();
+ 
+			System.out.println("Done" + partitionTable.toString());
+ 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ backend/kickStart.java
@@ -0,0 +1,22 @@
+package backend;
+
+import java.io.File;
+
+
+public class kickStart {
+	public static final int maxBlockSize = 500;	// in KB
+	public static final int diskSize = 10;			// in MB
+	
+	public static void main(String[] args) {
+		kickStart.chkDisk();
+		//start GUI
+	}
+	
+	private static void chkDisk(){
+		File disk = Disk.transDisk;
+		
+		if(!disk.exists()){
+			Disk.createDisk();
+		}
+	}
+}
