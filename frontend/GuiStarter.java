d38edc6b5f6a7659ef9572093ef7002dc0183fb1
harshad mulmuley <harshad.a.mulmuley@gmail.com>
2013-02-21 22:18:08 +0530

    text editor improvement

--- frontend/GuiStarter.java
+++ frontend/GuiStarter.java
@@ -2,7 +2,10 @@ package frontend;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
+import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -10,6 +13,7 @@ import javax.swing.JMenuItem;
 
 public class GuiStarter{
 	JFrame frame = new JFrame("Transparent OS");
+	JDesktopPane desktop = new JDesktopPane();
 	JMenuBar menuBar = new JMenuBar();
 	JMenu file = new JMenu("File");
 	JMenu edit = new JMenu("Edit");
@@ -19,6 +23,7 @@ public class GuiStarter{
 	public GuiStarter(){
 		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		frame.setVisible(true);
+		frame.add(desktop);
 		addMenuBar();
 		addMenuItems();
 	}
@@ -44,7 +49,7 @@ public class GuiStarter{
 		newFile.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				//call text editor here
-				new TextEditor();
+				desktop.add(new TextEditor());
 			}
 		});
 	}
--- frontend/TextEditor.java
+++ frontend/TextEditor.java
@@ -4,13 +4,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JTextArea;
 
-public class TextEditor {
-	JFrame frame = new JFrame("TEdit");
+public class TextEditor extends JInternalFrame{
 	JTextArea area =  new JTextArea();
 	JMenuBar menuBar = new JMenuBar();
 	JMenu file = new JMenu("File");
@@ -19,22 +19,25 @@ public class TextEditor {
 	JMenuItem exit = new JMenuItem("Exit");
 	
 	public TextEditor() {
-		frame.setBounds(50, 50, 500, 500);
-		frame.setVisible(true);
+		setTitle("TEdit");
+		setBounds(50, 50, 500, 500);
+		setVisible(true);
+		setClosable(true);
+		setMaximizable(true);
 		addMenuBar();
 		addMenuItems();
 		addTextArea();
 	}
 	
 	private void addTextArea(){
-		frame.add(area);		
+		add(area);		
 	}
 	
 	private void addMenuBar(){
-		frame.setJMenuBar(menuBar);
+		setJMenuBar(menuBar);
 		
 		menuBar.add(file);
-		menuBar.add(edit);		
+		menuBar.add(edit);
 	}
 	
 	private void addMenuItems(){
@@ -44,7 +47,7 @@ public class TextEditor {
 		//action listeners for menu items
 		exit.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent arg0) {
-				frame.dispose();
+				dispose();
 			}
 		});
 		
