3a4465cc46e8dfab742a721ff22d1c83fa2b82ca
harshad mulmuley <harshad.a.mulmuley@gmail.com>
2013-03-15 14:41:41 +0530

    added right-click functions, editable folder name etc

--- frontend/GuiStarter.java
+++ frontend/GuiStarter.java
@@ -2,16 +2,28 @@ package frontend;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import javax.swing.ImageIcon;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+
+import sun.org.mozilla.javascript.internal.ast.KeywordLiteral;
 
 public class GuiStarter {
 	JFrame mainFrame = new JFrame("Transparent OS");
@@ -22,17 +34,60 @@ public class GuiStarter {
 	JMenu newFileOrFolder = new JMenu("New");
 	JMenuItem newFile = new JMenuItem("File");
 	JMenuItem newFolder = new JMenuItem("Folder");
+	JPopupMenu popupMenu = new JPopupMenu("test");
 	JLabel[] lblArray = new JLabel[100]; 
 	static int count = 0;
 	
-	public GuiStarter() {
+	class PopupTriggerListener extends MouseAdapter{
+		public void mousePressed(MouseEvent e){
+			if(e.isPopupTrigger()){
+				popupMenu.show(e.getComponent(), e.getX(), e.getY());
+			}
+		}
+		public void mouseReleased(MouseEvent e){
+			if(e.isPopupTrigger()){
+				popupMenu.show(e.getComponent(), e.getX(), e.getY());
+			}
+		}
+	}
+	
+	class FolderNameEditListener extends MouseAdapter implements KeyListener{
+		public void mouseClicked(MouseEvent e){
+			e.getComponent().setEnabled(true);
+			e.getComponent().setBackground(mainPanel.getBackground());
+			e.getComponent().setForeground(Color.BLACK);
+		}
+	
+		@Override
+		public void keyPressed(KeyEvent e) {
+			if(e.getKeyCode() == KeyEvent.VK_ENTER){
+				e.getComponent().setEnabled(false);
+				e.getComponent().setBackground(mainPanel.getBackground());
+				e.getComponent().setForeground(Color.BLACK);
+			}
+		}
+		@Override
+		public void keyReleased(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public void keyTyped(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+	
+	public GuiStarter() {		
 		mainFrame.setExtendedState(Frame.MAXIMIZED_BOTH);
 		mainFrame.setVisible(true);
 		mainFrame.add(mainPanel);
 		mainPanel.setLayout(new BorderLayout());
 		mainPanel.add(mainMenuBar, BorderLayout.NORTH);
 		mainPanel.add(contentPanelWest,BorderLayout.WEST);
+		mainPanel.addMouseListener(new PopupTriggerListener());
 		addMenuItems();
+		addPopupMenuItems();
 	}
 	
 	private void addMenuItems(){
@@ -49,16 +104,42 @@ public class GuiStarter {
 		
 		newFolder.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				CreateFolder(count++);
+				createFolder(count++);
 			}
 		});
 	}
 	
-	private void CreateFolder(int count){
+	private void addPopupMenuItems(){
+		JMenuItem item1 = new JMenuItem("New File");
+		item1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				mainPanel.add(new TextEditor(mainFrame));
+			}
+		});
+		popupMenu.add(item1);
+		JMenuItem item2 = new JMenuItem("New Folder");
+		item2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				createFolder(count++);
+			}
+		});
+		popupMenu.add(item2);
+	}
+	
+	private void createFolder(int count){
 		ImageIcon icon = new ImageIcon("folder.gif");
-		lblArray[count] = new JLabel(icon);
-		lblArray[count].setText("new folder" + count);
-		contentPanelWest.add(lblArray[count]);
+		JLabel lbl = new JLabel(icon);
+		JTextField txt = new JTextField("new folder" + count);
+		txt.setEnabled(false);
+		txt.setBackground(mainPanel.getBackground());
+		txt.setDisabledTextColor(Color.BLACK);
+		FolderNameEditListener listener = new FolderNameEditListener();
+		txt.addMouseListener(listener);
+		txt.addKeyListener(listener);
+		//lbl.setText("\n"+"new folder" + count);
+		contentPanelWest.add(lbl);
+		contentPanelWest.add(txt);
 		contentPanelWest.revalidate();
 	}
+	
 }
